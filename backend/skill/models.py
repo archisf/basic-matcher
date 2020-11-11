f5b06b2f01a8753bd07317c3aee5498d45bb6c7b
archisf <arkadyaristov@gmail.com>
2020-11-11 16:42:23 +0200

    Create Skill app

---
+++ backend/skill/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'skill'
+        verbose_name = 'Skill'
+        verbose_name_plural = 'Skills'
