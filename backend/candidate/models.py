60980551c9ed4d97ece1b60bd7e9cc63d7336ed7
archisf <arkadyaristov@gmail.com>
2020-11-11 16:40:21 +0200

    Create Candidate app

---
+++ backend/candidate/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Candidate(models.Model):
+    title = models.CharField(max_length=100)
+    skill = models.ManyToManyField(
+        'skill.Skill',
+        verbose_name='skill',
+        blank=False,
+    )
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'candidate'
+        verbose_name = 'Candidate'
+        verbose_name_plural = 'Candidates'
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'config'
+    'config',
+    'candidate.apps.CandidateConfig',
+    'skill.apps.SkillConfig',
+    'job.apps.JobConfig'
 ]
 
 MIDDLEWARE = [
